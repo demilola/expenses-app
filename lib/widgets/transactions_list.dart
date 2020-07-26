87a79d734f2ec37935957e2d08363b49253b680b
Demilola Adefokun <d.adefokun@outlook.com>
2020-07-27 01:22:34 +0300

    added a flat delete button for wider screens

--- lib/widgets/transactions_list.dart
+++ lib/widgets/transactions_list.dart
@@ -16,18 +16,19 @@ class TransactionsList extends StatelessWidget {
     ThemeData theme = Theme.of(context);
     return transactions.isEmpty
         ? LayoutBuilder(
-                 builder: (ctx,constraints)=> Column(
+            builder: (ctx, constraints) => Column(
               children: [
                 Text('No Transactions Yet', style: theme.textTheme.headline5),
-                SizedBox(
-                    height: constraints.maxHeight*.005),
+                SizedBox(height: constraints.maxHeight * .005),
                 Expanded(
-                                  child: Image.asset('assets/images/waiting.png',
-                      fit: BoxFit.cover, ),
+                  child: Image.asset(
+                    'assets/images/waiting.png',
+                    fit: BoxFit.cover,
+                  ),
                 )
               ],
             ),
-        )
+          )
         : ListView.builder(
             shrinkWrap: true,
             itemCount: transactions.length,
@@ -52,7 +53,8 @@ class TransactionsList extends StatelessWidget {
                       leading: CircleAvatar(
                         radius: 30,
                         child: Padding(
-                          padding:  EdgeInsets.all(MediaQuery.of(context).size.width*.02),
+                          padding: EdgeInsets.all(
+                              MediaQuery.of(context).size.width * .02),
                           child: SizedBox(
                             height: MediaQuery.of(context).size.height * 0.03,
                             child: FittedBox(
@@ -67,12 +69,22 @@ class TransactionsList extends StatelessWidget {
                       subtitle: Text(
                         DateFormat.yMMMMd().format(transactions[index].date),
                       ),
-                      trailing: IconButton(
-                          icon: Icon(
-                            Icons.delete,
-                          ),
-                          onPressed: () =>
-                              deleteTransaction(transactions[index].id)),
+                      trailing: MediaQuery.of(context).size.width > 400
+                          ? FlatButton.icon(
+                              onPressed: () =>
+                                  deleteTransaction(transactions[index].id),
+                              icon: Icon(Icons.delete,
+                                  color: Theme.of(context).errorColor),
+                              label: Text('Delete'),
+                              textColor: Theme.of(context).errorColor,
+                            )
+                          : IconButton(
+                              icon: Icon(
+                                Icons.delete,
+                                color: Theme.of(context).errorColor,
+                              ),
+                              onPressed: () =>
+                                  deleteTransaction(transactions[index].id)),
                     ),
                   ));
             });
