06a39d600f18a720239c9884c2e8aae59df19d32
Demilola Adefokun <d.adefokun@outlook.com>
2020-07-26 23:35:03 +0300

    restructured bar chart with LayoutBuilder

--- lib/widgets/chart_bar.dart
+++ lib/widgets/chart_bar.dart
@@ -9,40 +9,42 @@ class ChartBar extends StatelessWidget {
   final double spendingPercentOfTotal;
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        SizedBox(
-            height: MediaQuery.of(context).size.height * 0.03,
-            child: FittedBox(
-                child: Text('\$${totalAmountSpent.toStringAsFixed(2)}'))),
-        SizedBox(height: MediaQuery.of(context).size.height * 0.0075),
-        Container(
-            height: 60,
-            width: 10,
-            child: Stack(
+    return LayoutBuilder(
+        builder: (context, constraints) => Column(
               children: [
+                SizedBox(
+                    height: constraints.maxHeight * .15,
+                    child: FittedBox(
+                        child:
+                            Text('\$${totalAmountSpent.toStringAsFixed(2)}'))),
+                SizedBox(height: constraints.maxHeight * .05),
                 Container(
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      border: Border.all(
-                        color: Colors.grey,
-                      )),
-                ),
-                FractionallySizedBox(
-                    heightFactor: spendingPercentOfTotal,
-                    child: Container(
-                      decoration: BoxDecoration(
-                        color: Theme.of(context).accentColor,
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                    ))
+                    height: constraints.maxHeight * .60,
+                    width: 10,
+                    child: Stack(
+                      children: [
+                        Container(
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              border: Border.all(
+                                color: Colors.grey,
+                              )),
+                        ),
+                        FractionallySizedBox(
+                            heightFactor: spendingPercentOfTotal,
+                            child: Container(
+                              decoration: BoxDecoration(
+                                color: Theme.of(context).accentColor,
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                            ))
+                      ],
+                    )),
+                SizedBox(height: constraints.maxHeight * .05),
+                Container(
+                    height: constraints.maxHeight * .15,
+                    child: FittedBox(child: Text(label.toUpperCase())))
               ],
-            )),
-        SizedBox(height: MediaQuery.of(context).size.height * 0.0075),
-        SizedBox(
-            height: MediaQuery.of(context).size.height * 0.03,
-            child: Text(label.toUpperCase()))
-      ],
-    );
+            ));
   }
 }
